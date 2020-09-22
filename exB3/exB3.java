46dcdd452706da6a852df4653bad18afa832a025
Philippe Peeters <philippe.a.peeters@bnpparibasfortis.com>
2020-09-22 13:38:29 +0200

    Challenge B3

--- exB3/exB3.java
+++ exB3/exB3.java
@@ -1,14 +1,35 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class exB3 {
 	
-   public static void main(String args[]){
-      int iterator = 0;
-	  int totalOfInterger = 0;
-      int intergerArray[] = {2,9,3,0,8};
-      int numberOfInteger = 5;
+	public static void main(String[] args) {
+		int totalOfInterger = 0;
+		ArrayList<Integer> intergerArray = new ArrayList<Integer>();
+		int numberOfInteger = 0;
+		String inputNumberStringUser = null;
+		boolean endEntry = true;
+	
+		do {
+			numberOfInteger++;
+			Scanner scanner = new Scanner( System.in );
+			System.out.print( "Enter numbers " + numberOfInteger +" (X to terminate) : " );
+			inputNumberStringUser = scanner.nextLine();
+			
+			try {  
+			    	Double.parseDouble(inputNumberStringUser);  
+			    	intergerArray.add(Integer.parseInt(inputNumberStringUser));
+			    	totalOfInterger += intergerArray.get(numberOfInteger - 1);
+			    	endEntry = true;
+			  } catch(NumberFormatException e){
+				  	endEntry = false;
+			  }  
+		} while (endEntry);
 
-      for(iterator=0; iterator < numberOfInteger; iterator++) {
-         totalOfInterger += intergerArray[iterator];
-      }
-      System.out.println("Average :"+ totalOfInterger/(float)numberOfInteger);
-   }
+		for (int number: intergerArray){
+			System.out.println("intergerArray : " + number);
+		}
+		System.out.println("Total : " + totalOfInterger);
+		System.out.println("Average :"+ totalOfInterger/(float)(numberOfInteger-1));
+	}
 }
