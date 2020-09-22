22172a9665eda2036090d4c639c05eb77bbe26ca
Philippe Peeters <philippe.a.peeters@bnpparibasfortis.com>
2020-09-22 09:54:15 +0200

    exA1 execice

---
+++ exA1/exA1.java
@@ -0,0 +1,20 @@
+
+public class exA1 {
+	public static void main(String[] args) {
+		String titleCourse = null;
+		int numberOfDayCourse = 0;
+		int priceOfCourse = 0;
+		boolean priorKnowledgeOfCourse = false;
+		
+		titleCourse = "Java Beginner";
+		numberOfDayCourse = 10;
+		priceOfCourse = 1500;
+		priorKnowledgeOfCourse = true;
+		
+		System.out.println("Title : " + titleCourse);
+		System.out.println("Number of day  : " + numberOfDayCourse);
+		System.out.println("Price : " + priceOfCourse);
+		System.out.println("Is prior knowledge needed : " + priorKnowledgeOfCourse);
+	}
+	
+}
