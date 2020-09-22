2ab524958844157af1679d6e2c58e511531b7c4a
Philippe Peeters <philippe.a.peeters@bnpparibasfortis.com>
2020-09-22 14:00:49 +0200

    Change Scanner and size of the array size()

--- exB3/exB3.java
+++ exB3/exB3.java
@@ -6,30 +6,31 @@ public class exB3 {
 	public static void main(String[] args) {
 		int totalOfInterger = 0;
 		ArrayList<Integer> intergerArray = new ArrayList<Integer>();
-		int numberOfInteger = 0;
 		String inputNumberStringUser = null;
 		boolean endEntry = true;
-	
+		Scanner scanner = new Scanner( System.in );
+		
 		do {
-			numberOfInteger++;
-			Scanner scanner = new Scanner( System.in );
-			System.out.print( "Enter numbers " + numberOfInteger +" (X to terminate) : " );
+			System.out.print( "Enter numbers (X to terminate) : " );
 			inputNumberStringUser = scanner.nextLine();
 			
 			try {  
 			    	Double.parseDouble(inputNumberStringUser);  
+			    	System.out.println("Number input : " + inputNumberStringUser);
 			    	intergerArray.add(Integer.parseInt(inputNumberStringUser));
-			    	totalOfInterger += intergerArray.get(numberOfInteger - 1);
 			    	endEntry = true;
 			  } catch(NumberFormatException e){
 				  	endEntry = false;
 			  }  
 		} while (endEntry);
 
-		for (int number: intergerArray){
-			System.out.println("intergerArray : " + number);
+		System.out.println("Size of the Array : " + intergerArray.size());
+		for (int i=0; i<intergerArray.size(); i++){
+			System.out.println("intergerArray number : " + intergerArray.get(i));
+	    	totalOfInterger += intergerArray.get(i);
 		}
+	
 		System.out.println("Total : " + totalOfInterger);
-		System.out.println("Average :"+ totalOfInterger/(float)(numberOfInteger-1));
+		System.out.println("Average :"+ totalOfInterger/(float)intergerArray.size());
 	}
 }
